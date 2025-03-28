bcfb5a09a3a84e375bd91451b7fa12de571ca4ba
itzzgimha <gimhandavinda65@gmail.com>
2025-03-28 12:43:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "සෙසන් දාන්න", // session id
+SESSION_ID: process.env.SESSION_ID || "SUPUN-MD=aJlTnIoJ#PQlW0As-0l_8JLp_b0ttlukqjL2fBfDsJ1vZTG_pTfY", // session id
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/5xzWmSxn/20250224-094453.png",
 ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
